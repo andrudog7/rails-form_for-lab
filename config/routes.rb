8732fa2f43646fdb7251e352b0b3a448fdd0a5de
--add <andrewksmoker@gmail.com>
2020-12-16 14:36:49 -0500

    commit tree 303444f584d36008bb4141894a062ab01c98e3e3

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
+  resources :students, only: [:create, :update, :new, :show, :edit]
+  resources :school_classes, only: [:create, :update, :new, :show, :edit]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
